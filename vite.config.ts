df094cde30b197413a21acf4e756da348b85a6af
Shaikh Parvez <shaikhparvez50@gmail.com>
2025-05-04 17:31:48 +0530

    solve the error fo vercel 4

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react({
-      jsxImportSource: 'react',
+      jsxRuntime: 'automatic',
       dev: mode === 'development',
     }),
     mode === 'development' && componentTagger(),
